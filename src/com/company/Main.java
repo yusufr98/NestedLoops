f863824ce4c1bb467294fbb50bcdc82935cc42c4
yusufr98 <yusuf090798@gmail.com>
2019-10-22 13:03:04 -0400

    initial

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //loop to iterate through numbers(1-9)
+        for(int i = 1; i < 10; i++){
+	        //loop to print numbers
+            for(int j = 0; j < i; j++){
+	        System.out.print(i);
+        }
+	    System.out.println();
+    }
+    }
+}
